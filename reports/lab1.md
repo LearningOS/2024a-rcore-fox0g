ea70a176898a79c78ae360987a48dca912c8370f
fox0g <1877691979@qq.com>
2024-10-31 11:36:59 +0800

    ch4 add lab1

---
+++ reports/lab1.md
@@ -0,0 +1,3 @@
+Rustsbi 版本为： 0.2.0-alpha.2
+
+[内核]应用程序中的 PageFault，错误的 addr = 0x0，错误的指令 = 0x804003a4，内核杀死了它。 [内核]IllegalInstruction 中，内核杀死了它。 [内核]IllegalInstruction 中，内核杀死了它。
---
+++ temp-os/src/syscall/process.rs
@@ -0,0 +1,108 @@
+//! Process management syscalls
+use crate::{
+    config::MAX_SYSCALL_NUM,
+    task::{
+        change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus,
+        get_syscall_times,get_syscall_begin,current_user_token,task_alloc_mmap,task_alloc_unmmap
+    },
+    timer::{
+        get_time_us,
+    },
+    mm::{
+        translated_ptr_pa
+    }
+};
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TimeVal {
+    pub sec: usize,
+    pub usec: usize,
+}
+
+/// Task information
+#[allow(dead_code)]
+pub struct TaskInfo {
+    /// Task status in it's life cycle
+    status: TaskStatus,
+    /// The numbers of syscall called by task
+    syscall_times: [u32; MAX_SYSCALL_NUM],
+    /// Total running time of task
+    time: usize,
+}
+
+/// task exits and submit an exit code
+pub fn sys_exit(_exit_code: i32) -> ! {
+    trace!("kernel: sys_exit");
+    exit_current_and_run_next();
+    panic!("Unreachable in sys_exit!");
+}
+
+/// current task gives up resources for other tasks
+pub fn sys_yield() -> isize {
+    trace!("kernel: sys_yield");
+    suspend_current_and_run_next();
+    0
+}
+
+/// YOUR JOB: get time with second and microsecond
+/// HINT: You might reimplement it with virtual memory management.
+/// HINT: What if [`TimeVal`] is splitted by two pages ?
+pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+    trace!("kernel: sys_get_time");
+    let us = get_time_us();
+    let ts = translated_ptr_pa(current_user_token(), _ts);
+    
+    *ts = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    
+    0
+}
+
+/// YOUR JOB: Finish sys_task_info to pass testcases
+/// HINT: You might reimplement it with virtual memory management.
+/// HINT: What if [`TaskInfo`] is splitted by two pages ?
+pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
+    trace!("kernel: sys_task_info NOT IMPLEMENTED YET!");
+    let ti = translated_ptr_pa(current_user_token(), _ti);
+
+    *ti = TaskInfo {
+        status: TaskStatus::Running,
+        syscall_times: get_syscall_times(),
+        time: get_syscall_begin(),
+    };
+    
+    0
+}
+
+// YOUR JOB: Implement mmap.
+pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
+    if _len == 0 {
+        return 0;
+    }
+    if _port & !0x7 != 0 || _port & 0x7 == 0 {
+        return -1;
+    }
+    trace!("kernel: sys_mmap NOT IMPLEMENTED YET!");
+
+    task_alloc_mmap(_start, _len, _port)
+
+}
+
+// YOUR JOB: Implement munmap.
+pub fn sys_munmap(_start: usize, _len: usize) -> isize {
+    trace!("kernel: sys_munmap NOT IMPLEMENTED YET!");
+    task_alloc_unmmap(_start, _len)
+
+}
+/// change data segment size
+pub fn sys_sbrk(size: i32) -> isize {
+    trace!("kernel: sys_sbrk");
+    if let Some(old_brk) = change_program_brk(size) {
+        old_brk as isize
+    } else {
+        -1
+    }
+}
